9508cdcf24f1c0916e5df6a3d5a74bec4a61dd13
Matthias <lenz_m497@hotmail.de>
2019-06-11 10:30:45 +0100

    Adding a board size singleton and tweaking food consumption reward

--- src/main/java/mcts/MCTSNode.java
+++ src/main/java/mcts/MCTSNode.java
@@ -34,7 +34,7 @@ public class MCTSNode implements Comparable{
 		advanceGameToNode(gameState, me, opponentBot);
 		runGame(gameState, me, opponentBot);
 //		double score = snakesBeatenBonus(snakesStart, gameState.heads.size());
-		double score = me.foodEaten > foodEatenStart ? 50 : 0;
+		double score = me.foodEaten > foodEatenStart ? (100/snakesStart) : 0;
 		if(isGameWon(gameState, me)) {
 			score += 100;
 		}
